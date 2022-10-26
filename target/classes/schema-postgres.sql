77c195f7ef942cd5b7c786aa409ffc3680fb3079
Benoit Moussaud <benoit@moussaud.org>
2022-10-26 17:14:21 +0200

    tanzuignore

--- target/classes/schema-postgres.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Bird (
-    index SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    type VARCHAR(255),
-    age integer,
-    url VARCHAR(255),
-    uri VARCHAR(255),
-    hostname VARCHAR(255));
